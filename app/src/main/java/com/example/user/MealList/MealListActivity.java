abb238a4e8a608a5edc569f750aa3b5c3e88bdda
Nasruddin91 <nasruddin_work@hotmail.com>
2015-05-10 01:52:00 +0800

    finished meal list activity

---
+++ app/src/main/java/com/example/user/MealList/MealListActivity.java
@@ -0,0 +1,65 @@
+package com.example.user.MealList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.example.user.OrderList.OrderListAdapter;
+import com.example.user.pushnotificationexperiment.R;
+import com.example.user.updating_service.UpdateService;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+public class MealListActivity extends Activity {
+
+    private ListView listView;
+    private MealListAdapter mealListAdapter;
+    private String seller_id = "4J9ILTJJq9" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_list);
+
+        listView = (ListView) findViewById(R.id.list);
+        mealListAdapter = new MealListAdapter(this);
+        listView.setAdapter(mealListAdapter);
+        Intent intent = new Intent(this, UpdateService.class);
+        startService(intent);
+
+        ParseQuery<ParseObject> queryAllOrders = ParseQuery.getQuery("Meal");
+        queryAllOrders.whereEqualTo("sellerId", ParseObject.createWithoutData("Seller", seller_id) );
+        queryAllOrders.include("mealId");
+        queryAllOrders.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e != null) {
+                    // Handle exception
+                    Log.e("Meal", e.getMessage());
+                    return;
+                }else{
+                    Log.d("Meal", "e is null");
+                }
+
+                Log.d("Parse", "Retrieved " + list.size() + " orders");
+                for (int i = 0; i < list.size(); i++) {
+                    Log.d("Parse", "Meal " + i + ": " + list.get(i).get("name"));
+                }
+
+                mealListAdapter.clear();
+                mealListAdapter.addAll(list);
+                mealListAdapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/user/MealList/MealListAdapter.java
@@ -0,0 +1,121 @@
+package com.example.user.MealList;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.example.user.pushnotificationexperiment.R;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+/**
+ * Created by User on 9/5/2015.
+ */
+public class MealListAdapter extends ArrayAdapter<ParseObject> {
+
+    private final Context ctx;
+
+    public MealListAdapter(Context context) {
+        super(context, 0);
+        this.ctx = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View layout;
+
+        layout = LayoutInflater.from(getContext()).inflate(R.layout.meal_list_item, null);
+
+        final ParseObject meal = getItem(position);
+
+        TextView mealName = (TextView) layout.findViewById(R.id.mealname);
+        final Button toggleButton = (Button) layout.findViewById(R.id.stockToggleButton);
+
+        final String mealIsSoldOut = meal.get("isSoldOut").toString();
+        if (mealIsSoldOut == "true"){
+            toggleButton.setText("Sold Out");
+        }else{
+            toggleButton.setText("In Stock");
+        }
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mealIsSoldOut == "true") {
+                    //set meal isSoldOut to false
+                    toggleButton.setText("In Stock");
+                    setMealIsSoldOut(meal.getObjectId(), false );
+                } else {
+                    //set meal isSoldOut to true
+                    toggleButton.setText("Sold Out");
+                    setMealIsSoldOut(meal.getObjectId(), true);
+                }
+            }
+        });
+
+        String mealname = meal.get("name").toString();
+
+        mealName.setText(mealname);
+
+        return layout;
+    }
+
+    public void setMealIsSoldOut(String mealId, final boolean isSoldOut){
+        final ParseQuery<ParseObject> queryMeals = ParseQuery.getQuery("Meal");
+        queryMeals.getInBackground(mealId, new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, ParseException e) {
+                parseObject.put("isSoldOut", isSoldOut);
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        resetView();
+                    }
+                });
+            }
+        });
+    }
+
+    public void resetView(){
+        ParseQuery<ParseObject> queryAllOrders = ParseQuery.getQuery("Meal");
+        queryAllOrders.whereEqualTo("sellerId", ParseObject.createWithoutData("Seller", "4J9ILTJJq9") );
+        queryAllOrders.include("mealId");
+        queryAllOrders.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e != null) {
+                    // Handle exception
+                    Log.e("Meal", e.getMessage());
+                    return;
+                }else{
+                    Log.d("Meal", "e is null");
+                }
+
+                Log.d("Parse", "Retrieved " + list.size() + " orders");
+                for (int i = 0; i < list.size(); i++) {
+                    Log.d("Parse", "Meal " + i + ": " + list.get(i).get("name"));
+                }
+
+                clear();
+                addAll(list);
+                notifyDataSetChanged();
+            }
+        });
+
+
+    }
+}
